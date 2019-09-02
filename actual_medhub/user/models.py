5b6b561cdc90271e1a41504a57e2d18966f5b11e
jenishakhadka <54480807+jenishakhadka@users.noreply.github.com>
2019-09-02 12:30:40 +0545

    Updated models.py
    -Added UserDetails table
    -Added Category table

--- actual_medhub/user/models.py
+++ actual_medhub/user/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserDetails(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    age = models.IntegerField()
+    is_cust = models.BooleanField(default=True)
+    is_delivery = models.BooleanField(default=False)
+
+class Category(models.Model):
+    name = models.CharField(max_length = 40)
+
+class product(models.Model):
+     product_name = models.CharField(max_length = 100)
+     category     = models.ForeignKey(Category, on_delete = models.CASCADE)
+     price        = models.IntegerField()
+     mfg_date     = models.DateTimeField()
+     exp_date     = models.DateTimeField()
+     dosage       = models.IntegerField()
