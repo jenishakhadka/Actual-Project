80d87ac20c533761d481bd1717d526450ab90401
keeran2488 <49069135+keeran2488@users.noreply.github.com>
2019-09-03 19:39:38 +0545

    Added signup form

--- actual_medhub/actual_medhub/urls.py
+++ actual_medhub/actual_medhub/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from user.views import SignUpView, signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
     path('users/', include('django.contrib.auth.urls')),
+    path('signup/', signup, name='signup'),
 ]
-
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
--- actual_medhub/user/views.py
+++ actual_medhub/user/views.py
@@ -1,17 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import reverse_lazy
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
+from django.shortcuts import redirect
 
 
-from .models import UserDetails, product
-from .forms import CustomUserCreationForm
+from .models import product
+# from .forms import CustomUserCreationForm
 
 
 class SignUpView(CreateView):
-    form_class = CustomUserCreationForm
+    form_class = UserCreationForm
     sucess_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form':form})
+        
 
 
 def HomeView(request):
